140bf2e93b3dbe151bd526e791df82c76019ba36
brunouriques <92872292+brunouriques@users.noreply.github.com>
2023-01-04 05:09:19 -0300

    Update code to support gender selection and future calc change deppending on gender

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Começo seleção opção
+
+
+
+
+
+
+
+
+// escolher genero
+// com base no genero, aparece uma opção de NAF
+// metodo hide?
+
+// Fim seleção opção
+
 //Começo EER
 
 function getNAF() {
@@ -5,17 +20,20 @@ function getNAF() {
   naf = select.value;
   console.log(naf);
   naf = Number(naf);
-} 
+}
+
 function getI() {
   i = document.getElementById('iId').value;
   console.log(i);
   i = Number(i);
 }
+
 function getP() {
   p = document.getElementById('pId').value;
   console.log(p);
   p = Number(p);
 }
+
 function getE() {
   e = document.getElementById('eId').value;
   console.log(e);
@@ -23,51 +41,56 @@ function getE() {
 }
 
 function calc() {
-  total = 354 - (6.91 * i) + naf * (9.36 * p) + (726 * e);
-  total = total.toFixed(2);
-  document.querySelector('.outputEER').textContent= total;
+  gender = document.getElementById('selecGender').value;
+  cosole.log(gender)
+  if (gender == "m") {
+    total = 662 - (9.53 * i) + naf * (15.91 * p) + (539.6 * e);
+    total = total.toFixed(2);
+    document.querySelector('.outputEER').textContent = total;
+
+  } else {
+    total = 354 - (6.91 * i) + naf * (9.36 * p) + (726 * e);
+    total = total.toFixed(2);
+    document.querySelector('.outputEER').textContent = total;
+  }
 }
 
 // Fim EER
 
 // Começo IMC
 
-function calcPeso(){
+function calcPeso() {
   peso = document.getElementById('recebePeso').value;
   peso = Number(peso);
   console.log(peso);
 }
-function calcAltura(){
+
+function calcAltura() {
   altura = document.getElementById('recebeAltura').value;
   altura = Number(altura);
   console.log(altura);
 }
-function calcIMC(){
-  imc = peso/Math.pow(altura, 2);
+
+function calcIMC() {
+  imc = peso / Math.pow(altura, 2);
   if (imc < 18.5) {
     clas = ". Você está abaixo do peso.";
-  }
-  else if (imc > 18.5 && imc < 24.9) {
+  } else if (imc > 18.5 && imc < 24.9) {
     clas = ". Você está com peso normal."
-  }
-  else if (imc > 25 && imc < 29.9) {
+  } else if (imc > 25 && imc < 29.9) {
     clas = ". Você está com sobrepeso."
-  }
-  else if (imc > 30 && imc < 34.9) {
+  } else if (imc > 30 && imc < 34.9) {
     clas = ". Você está com obesidade grau I."
-  }
-  else if (imc > 35 && imc < 39.9) {
+  } else if (imc > 35 && imc < 39.9) {
     clas = ". Você está com obesidade grau II."
-  }
-  else if (imc >= 40) {
+  } else if (imc >= 40) {
     clas = ". Você está com obesidade grau III."
-  }
-  else {
+  } else {
     clas = " (Valor inválido.)";
   }
   imc = imc.toFixed(2);
   console.log(imc);
-  document.querySelector('.outputIMC').textContent = imc + clas ;
+  document.querySelector('.outputIMC').textContent = imc + clas;
 }
 
 // Fim IMC
